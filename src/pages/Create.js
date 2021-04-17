0aafe1a8fe0c16844cdff7a5cc8b69ba5260272f
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-04-17 10:34:10 +0300

    makeStyles hook

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,10 +1,22 @@
 import React from 'react'
-import { Typography, Button, ButtonGroup, Container } from '@material-ui/core'
+import { Typography, Button, makeStyles, Container } from '@material-ui/core'
 import AcUnitIcon from '@material-ui/icons/AcUnit';
 import SendIcon from '@material-ui/icons/Send';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
+let useStyles = makeStyles( {
+  btn: {
+    backgroundColor: 'orange',
+    fontSize: 30,
+    '&:hover': { backgroundColor: 'red' }
+  }
+} )
+
+
 export default function Create() {
+
+  let classes = useStyles()
+
   return (
     <Container>
       <Typography
@@ -18,17 +30,12 @@ export default function Create() {
       </Typography>
 
       <Button
+        className={ classes.btn }
         variant='contained'
         endIcon={<ArrowForwardIosIcon />}
         onClick={() => console.log('I am clicked')
         }>consol log</Button>
-      <br />
-      <AcUnitIcon />
-      <AcUnitIcon color='secondary' fontSize='large' />
-      <AcUnitIcon color='secondary' fontSize='small' />
-      <AcUnitIcon color='action' fontSize='small' />
-      <AcUnitIcon color='error' fontSize='small' />
-      <AcUnitIcon color='disabled' fontSize='small' />
+      
     </Container>
   )
 }
