59752f29e8f9a50299b236af210305d5838c36f9
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-04-18 20:52:25 +0300

    list and list items

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,7 @@
 import { makeStyles, Drawer, Typography } from "@material-ui/core";
+import { List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
+import { AddCircleOutlineOutlined, SubjectOutlined } from "@material-ui/icons";
+import { useHistory } from 'react-router'
 
 let drawerWidth = 240
 
@@ -22,6 +25,19 @@ let useStyle = makeStyles({
 const Layout = ({ children }) => {
 
   let classes = useStyle()
+  let history = useHistory()
+  let menuItem = [
+    {
+      text: 'My notes',
+      icon: <SubjectOutlined color='secondary' />,
+      path: '/'
+    },
+    {
+      text: 'Create Icon',
+      icon: <AddCircleOutlineOutlined color='secondary' />,
+      path: '/create'
+    }
+  ]
 
   return (
     <div className={ classes.root }>
@@ -31,12 +47,25 @@ const Layout = ({ children }) => {
         anchor='left'
         classes={ { paper: classes.drawerPaper } }
       >
-        <Typography variant='h5'>
-          Ninja Notes
-        </Typography>
+        <div>
+          <Typography variant='h5'>
+            Ninja Notes
+          </Typography>
+        </div>
+        <List>
+          { menuItem.map(item => (            
+            <ListItem
+              button
+              key={ item.text } 
+              onClick={ () => history.push(item.path) }
+            >
+              <ListItemIcon>{ item.icon }</ListItemIcon>
+              <ListItemText primary={ item.text } />
+            </ListItem>
+          )) }
+        </List>
       </Drawer>
-
-
+      
       <div className={classes.page}>
         {children}
       </div>
